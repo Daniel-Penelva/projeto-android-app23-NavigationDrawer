4e718b60bd373a68ac338e79dde51fd9e345c8ee
Daniel Penelva <d4n.andrade@gmail.com>
2023-04-28 03:31:39 -0300

    Conhecendo a estrutura do Navigation Drawer

--- app/src/main/java/com/example/navigationdrawer/MainActivity.java
+++ app/src/main/java/com/example/navigationdrawer/MainActivity.java
@@ -30,26 +30,39 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Configura barra de navegação
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        // Cria referência de toda a área de NavigationDrawer
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
+
+        /*Cria a referência para a área de navegação - é toda a área que ao clicar no botão das três linhas horizontais no
+        canto superior da app, no caso, o botão fica no toolbar.*/
         NavigationView navigationView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+
+        /* Define configurações do NavigationDrawer */
         mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
                 R.id.nav_tools, R.id.nav_share, R.id.nav_send)
                 .setDrawerLayout(drawer)
                 .build();
+
+        /*Configura área que irá carregar os fragments - é dentro dele que vai ser carregado os conteúdos
+        que fica dentro do content_main.xml - o id nav_host_fragment leva a esse layout. */
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+
+        /* Exibe toda a área NavigationView quando se clica no botão com as três linhas horizontais no canto superior do
+        app. Ou seja configura menu superior de navegação.  */
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+
+        // Configura a navegação para o NavigationView
         NavigationUI.setupWithNavController(navigationView, navController);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+        // Inflar o menu; isso adiciona itens à barra de ação, se estiver presente.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
@@ -61,3 +74,22 @@ public class MainActivity extends AppCompatActivity {
                 || super.onSupportNavigateUp();
     }
 }
+
+
+/* Anotações:
+*
+* Vamos criar um item de menu. Na pasta Menu no arquivo 'main.xml' vamos copiar e colar a tag item
+* para criar um item de menu 'Sair'. Lembrando que vamos desenvolver usando o xml (aba Text).
+*
+* Vale ressaltar que o método onCreateOptionsMenu (ActivityMain) é configurar o menu daa toolbar,
+* no caso, 'main.xml' e o 'activity_main_drawer.xml' (pasta menu). É possível reparar que o main.xml
+* é carregado dentro desse método.
+*
+* --------------------------------------------
+* Dentro do Builder no mAppBarConfiguration é passado os itens de navegação que vai ser utilizado,
+* por exxemplo, os itens inicial, galeria, apresentação, ferramentas, compartilhar e enviar.
+*
+* -------------------------------------------
+* A pasta navigation define as navegações dos itens da NavigationView, são os itens inicial, galeria ...
+*
+* */
